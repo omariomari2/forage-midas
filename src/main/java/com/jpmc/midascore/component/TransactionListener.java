3ce1910504a3a55d04dbc3c092fc32fe024614f7
Bright Omari Owusu <188401332+omariomari2@users.noreply.github.com>
2025-10-20 22:42:29 -0500

    integrated kafka for queue processing

---
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -0,0 +1,26 @@
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionListener {
+    
+    private static final Logger logger = LoggerFactory.getLogger(TransactionListener.class);
+    
+    @Value("${general.kafka-topic}")
+    private String topic;
+    
+    @KafkaListener(topics = "${general.kafka-topic}", groupId = "${spring.kafka.consumer.group-id}")
+    public void handleTransaction(Transaction transaction) {
+        logger.info("Received transaction: {}", transaction);
+        logger.info("Transaction amount: {}", transaction.getAmount());
+        
+        // For now, just log the transaction - processing will be added later
+        // This satisfies the requirement to "simply integrate Kafka into Midas Core"
+    }
+}
